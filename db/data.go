0b7c096f897c9ab59607b66d27d1c59f3ee70625
danik-progger <lyatorovskiy@yandex.ru>
2025-05-07 18:31:10 +0300

    Added tests for sql and reddis

--- db/data.go
+++ db/data.go
@@ -26,10 +26,40 @@ func GetUsers() map[string]*blog.User {
 			NickName: "satoru_gojo",
 			PhotoUrl: "https://satoru-photo.jpg",
 		},
+		"user-5": {
+			Id:       "user-5",
+			NickName: "katakura_ken",
+			PhotoUrl: "https://katakura-photo.jpg",
+		},
+		"user-6": {
+			Id:       "user-6",
+			NickName: "eren_yeager",
+			PhotoUrl: "https://eren-photo.jpg",
+		},
+		"user-7": {
+			Id:       "user-7",
+			NickName: "kaneki_ken",
+			PhotoUrl: "https://eren-photo.jpg",
+		},
+		"user-8": {
+			Id:       "user-8",
+			NickName: "izuki_midoriya",
+			PhotoUrl: "https://midoriya-photo.jpg",
+		},
+		"user-9": {
+			Id:       "user-9",
+			NickName: "ichigo_kurosaki",
+			PhotoUrl: "https://ichigo-photo.jpg",
+		},
+		"user-10": {
+			Id:       "user-10",
+			NickName: "son_goku",
+			PhotoUrl: "https://goku-photo.jpg",
+		},
 	}
 }
 
-func getUserById(id string) *blog.User {
+func GetUserById(id string) *blog.User {
 	return GetUsers()[id]
 }
 
@@ -37,7 +67,7 @@ func GetPosts() []*blog.Post {
 	return []*blog.Post{
 		{
 			Id:         "post-1",
-			Author:     getUserById("user-1"),
+			Author:     GetUserById("user-1"),
 			Body:       "Post 1 by Naruto!",
 			CreatedAt:  "13:11:00 26.03.2025",
 			LikesCount: 0,
@@ -45,7 +75,7 @@ func GetPosts() []*blog.Post {
 		},
 		{
 			Id:         "post-2",
-			Author:     getUserById("user-2"),
+			Author:     GetUserById("user-2"),
 			Body:       "Post 2 by Tanjiro!",
 			CreatedAt:  "09:00:00 13.01.2025",
 			LikesCount: 0,
@@ -53,7 +83,7 @@ func GetPosts() []*blog.Post {
 		},
 		{
 			Id:         "post-3",
-			Author:     getUserById("user-1"),
+			Author:     GetUserById("user-1"),
 			Body:       "Post 3 by Naruto!",
 			CreatedAt:  "21:00:00 31.12.2024",
 			LikesCount: 0,
@@ -61,8 +91,56 @@ func GetPosts() []*blog.Post {
 		},
 		{
 			Id:         "post-4",
-			Author:     getUserById("user-4"),
+			Author:     GetUserById("user-4"),
 			Body:       "Post 4 by Satoru!",
+			CreatedAt:  "17:31:00 03.09.2024",
+			LikesCount: 0,
+			IsLiked:    false,
+		},
+		{
+			Id:         "post-5",
+			Author:     GetUserById("user-5"),
+			Body:       "Post 5 by Katacura!",
+			CreatedAt:  "17:31:00 03.09.2024",
+			LikesCount: 0,
+			IsLiked:    false,
+		},
+		{
+			Id:         "post-6",
+			Author:     GetUserById("user-6"),
+			Body:       "Post 6 by Eren!",
+			CreatedAt:  "17:31:00 03.09.2024",
+			LikesCount: 0,
+			IsLiked:    false,
+		},
+		{
+			Id:         "post-7",
+			Author:     GetUserById("user-7"),
+			Body:       "Post 7 by Kaneki!",
+			CreatedAt:  "17:31:00 03.09.2024",
+			LikesCount: 0,
+			IsLiked:    false,
+		},
+		{
+			Id:         "post-8",
+			Author:     GetUserById("user-8"),
+			Body:       "Post 8 by Izuki!",
+			CreatedAt:  "17:31:00 03.09.2024",
+			LikesCount: 0,
+			IsLiked:    false,
+		},
+		{
+			Id:         "post-9",
+			Author:     GetUserById("user-9"),
+			Body:       "Post 9 by Ichigo!",
+			CreatedAt:  "17:31:00 03.09.2024",
+			LikesCount: 0,
+			IsLiked:    false,
+		},
+		{
+			Id:         "post-10",
+			Author:     GetUserById("user-10"),
+			Body:       "Post 10 by Goku!",
 			CreatedAt:  "17:31:00 03.09.2024",
 			LikesCount: 0,
 			IsLiked:    false,
--- go.mod
+++ go.mod
@@ -3,25 +3,34 @@ module go_grpc_blog
 go 1.23.4
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/go-redis/redismock/v8 v8.11.5
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3
+	github.com/stretchr/testify v1.8.1
 	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb
 	google.golang.org/grpc v1.71.0
 	google.golang.org/protobuf v1.36.6
+	gorm.io/gorm v1.25.10
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
-	gorm.io/gorm v1.25.10 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
